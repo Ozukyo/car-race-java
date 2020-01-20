f492586d4d86c07ed9c004372406ae6169f0a9f9
Ozukyo <ozukyo@gmail.com>
2020-01-20 21:20:06 +0100

    implemented Weather class with setRaining, isRaining and getRandomPercentage method

---
+++ Weather.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Weather {
+    private boolean raining;
+
+    public void setRaining() {
+        double randomNumber = getRandomPercentage();
+        if (randomNumber <= 0.30) {
+            raining = true;
+        } else if (randomNumber>0.30) {
+            raining = false;
+        }
+        
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public double getRandomPercentage() {
+        Random random = new Random();
+        double randomPercentage = random.nextDouble();
+        return randomPercentage;
+    }
+
+    
+}   
