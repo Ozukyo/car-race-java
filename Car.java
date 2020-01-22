ab516fd399e159c45c874b15707be922169c105c
Ozukyo <ozukyo@gmail.com>
2020-01-22 11:42:21 +0100

    changed methods in Car class

--- Car.java
+++ Car.java
@@ -2,13 +2,27 @@ import java.util.Random;
 
 public class Car {
     private int normalSpeed;
+    private int actualSpeed;
+    private int distanceTraveled = 0;
     private String name;
 
+    public Car() {
+        this.name = generateRandomName();
+        normalSpeed = getRandomSpeed();
+        actualSpeed = 0;
+    }
 
-    public int getRandomSpeed(){
-        Random random = new Random();
-        int randomSpeed = random.nextInt(110- 80 +1) + 80;
-        return randomSpeed;
+    public void moveForAnHour() {
+        distanceTraveled += actualSpeed;
+    }
+
+    public void prepareForLap(Race race) {
+        if(race.isThereABrokenTruck()) {
+            actualSpeed = 75;
+        }
+        else {
+            actualSpeed = normalSpeed;
+        }
     }
 
     public int getSpeed() {
@@ -16,16 +30,29 @@ public class Car {
         return normalSpeed;
     }
 
-    public void generateRandomName() {
+    public String getName() {
+        return name;
+    }
+
+    public String speedToString() {
+        return normalSpeed + "km\\h";
+    }
+
+    public int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    private String generateRandomName() {
         String[] carNames = {"Roamer", "Temperament", "Lioness", "Aeon", "Freedom", 
                             "Umbra", "Silver", "Spirit", "Tracer", "Patron"};
         Random rand = new Random();
         int index = rand.nextInt(carNames.length);
-        name = carNames[index];
+        return carNames[index];
     }
 
-    public String getName() {
-        generateRandomName();
-        return name;
+    private int getRandomSpeed(){
+        Random random = new Random();
+        int randomSpeed = random.nextInt(110- 80 +1) + 80;
+        return randomSpeed;
     }
 }
